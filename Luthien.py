ce4c725825ff77a04f2322238be61a4a5662a73b
zschaub <bdzschau@gmail.com>
2019-11-09 15:30:33 -0500

    Changed Variables to Global Variables

--- Luthien.py
+++ Luthien.py
@@ -6,21 +6,25 @@ from item import *
 def d6():
     return random.randint(1, 6)
 
+max_skill = 6 + d6()
+skill = max_skill
+max_health = 12 + d6() + d6()
+health = max_health
+max_luck = 6 + d6()
+luck = max_luck
+max_fuel = 6 + d6()
+fuel = max_fuel
+weapon = LaserMk1
+money = 50
 
 def main():
     print("Welcome to Luthien a text based space adventure game!")
     print("First we will create your character and ship")
-    max_skill = 6 + d6()
-    skill = max_skill
-    max_health = 12 + d6() + d6()
-    health = max_health
-    max_luck = 6 + d6()
-    luck = max_luck
-    max_fuel = 6 + d6()
-    fuel = max_fuel
-    money = 50
-    print("Skill: {skill} Health: {health} Luck: {luck} Fuel: {fuel}"
-          .format(skill=skill, health=health, luck=luck, fuel=fuel))
+
+
+    print(
+        "Skill: {skill} Health: {health} Luck: {luck} Fuel: {fuel}"
+            .format(skill=skill, health=health, luck=luck, fuel=fuel))
 
     print(
         "\nYou find yourself the captain of the Starship Luthien. Congratulations on your promotion!\nAs i'm sure you "
@@ -29,7 +33,6 @@ def main():
         "your goal is simple, navigate the various star systems of the universe and find and destroy the Eridu "
         "Capital ship, simple stuff for a captain such as yourself.\nDuring your journey you will encounter many "
         "things so don't be afraid to fail. Though i'm sure somebody with your skills will be fine. Good luck!")
-
 
 if __name__ == "__main__":
     main()
