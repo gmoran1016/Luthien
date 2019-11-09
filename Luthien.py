6866c919fd478c873dd915ee47bb9a6132737251
Griffin Moran <gmoran1016@gmail.com>
2019-11-09 14:21:14 -0500

    Intro

--- Luthien.py
+++ Luthien.py
@@ -13,6 +13,9 @@ def main():
     luck = 6 + d6()
     fuel = 6 + d6()
     print("Skill: {skill} Stamina: {stamina} Luck: {luck} Fuel: {fuel}".format(skill=skill, stamina=stamina, luck=luck, fuel=fuel))
+    
+    print("\nYou find yourself the captain of the Starship Luthien. Congratulations on your promotion!\nAs i'm sure you are aware you are on the run from the Eridu Empire, The most evil empire in the galaxy")
+    print("your goal is simple, navigate the various star systems of the universe and find and destroy the Eridu Capital ship, simple stuff for a captain such as yourself.\nDuring your journey you will encounter many things so don't be afraid to fail. Though i'm sure somebody with your skills will be fine. Good luck!")
 
 
 if __name__ == "__main__":
