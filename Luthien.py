036658e8b59a54b8812943435789646c43cd806a
Griffin Moran <gmoran1016@gmail.com>
2019-11-09 13:42:10 -0500

    Added Random gen

--- Luthien.py
+++ Luthien.py
@@ -1,8 +1,12 @@
-
+import random
 skill = 6
 stamina = 12
 luck = 6
 fuel = 6
+
+def d6():
+    d6var = random.randint(1,6)
+    return(d6var)
 
 def main():
     print("Welcome to Luthien a text based space adventure game!")
