454cf77de80d7aa71cde6706d7fcf3e3c2ab29e4
Griffin Moran <gmoran1016@gmail.com>
2019-11-09 14:34:48 -0500

    Pep 8 formatting

--- Luthien.py
+++ Luthien.py
@@ -2,8 +2,9 @@ import random
 
 
 def d6():
-    d6var = random.randint(1,6)
-    return(d6var)
+    d6var = random.randint(1, 6)
+    return (d6var)
+
 
 def main():
     print("Welcome to Luthien a text based space adventure game!")
@@ -17,9 +18,16 @@ def main():
     max_fuel = 6 + d6()
     fuel = max_fuel
     money = 50
-    print("Skill: {skill} Health: {health} Luck: {luck} Fuel: {fuel}".format(skill=skill, health=health, luck=luck, fuel=fuel))
-    print("\nYou find yourself the captain of the Starship Luthien. Congratulations on your promotion!\nAs i'm sure you are aware you are on the run from the Eridu Empire, The most evil empire in the galaxy")
-    print("your goal is simple, navigate the various star systems of the universe and find and destroy the Eridu Capital ship, simple stuff for a captain such as yourself.\nDuring your journey you will encounter many things so don't be afraid to fail. Though i'm sure somebody with your skills will be fine. Good luck!")
+    print("Skill: {skill} Health: {health} Luck: {luck} Fuel: {fuel}".format(skill=skill, health=health, luck=luck,
+                                                                             fuel=fuel))
+    print(
+        "\nYou find yourself the captain of the Starship Luthien. Congratulations on your promotion!\nAs i'm sure you "
+        "are aware you are on the run from the Eridu Empire, The most evil empire in the galaxy")
+    print(
+        "your goal is simple, navigate the various star systems of the universe and find and destroy the Eridu "
+        "Capital ship, simple stuff for a captain such as yourself.\nDuring your journey you will encounter many "
+        "things so don't be afraid to fail. Though i'm sure somebody with your skills will be fine. Good luck!")
+
 
 if __name__ == "__main__":
     main()
