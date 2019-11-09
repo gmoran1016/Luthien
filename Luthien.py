93c70133369a61bbfc574e869ec6b365b041fd87
zschaub <bdzschau@gmail.com>
2019-11-09 12:53:10 -0500

    Start of main

--- Luthien.py
+++ Luthien.py
@@ -1,4 +1,13 @@
+
+skill = 6
+stamina = 12
+luck = 6
+fuel = 6
+
 def main():
+    print("Welcome to Luthien a text based space adventure game!")
+    print("First we will create your character and ship")
+    
 
 if __name__ == "__main__":
     main()
