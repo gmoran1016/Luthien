2d589954aa1347cbfcb0ebff5d6d25c4dad5833c
zschaub <bdzschau@gmail.com>
2019-11-09 17:53:09 -0500

    Fixed EVERYTHING(but actually just store)

--- Luthien.py
+++ Luthien.py
@@ -8,36 +8,24 @@ def d6():
     return random.randint(1, 6)
 
 
-class Player():
-    def ___ini___(self, skill, health, luck, fuel, weapon, money, system, area):
-        self.max_skill = skill
-        self.skill = skill
-        self.max_health = health
-        self.health = health
-        self.max_luck = luck
-        self.max_fuel = fuel
-        self.fuel = fuel
-        self.weapon = weapon
-        self.money = money
-        self.system = system
-        self.area = area
 
-
-max_skill = 6 + d6()
-skill = max_skill
-max_health = 12 + d6() + d6()
-health = max_health
-max_luck = 6 + d6()
-luck = max_luck
-max_fuel = 6 + d6()
-fuel = max_fuel
-weapon = LaserMk1
-money = 50
-system = 1
-area = 1
 
 
 def main():
+    max_skill = 6 + d6()
+    skill = max_skill
+    max_health = 12 + d6() + d6()
+    health = max_health
+    max_luck = 6 + d6()
+    luck = max_luck
+    max_fuel = 6 + d6()
+    fuel = max_fuel
+    weapon = LaserMk1
+    money = 50
+    system = 1
+    area = 1
+    repairToolAmount = 1
+
     print("Welcome to Luthien a text based space adventure game!")
     print("First we will create your character and ship")
 
@@ -52,16 +40,28 @@ def main():
         "your goal is simple, navigate the various star systems of the universe and find and destroy the Eridu "
         "Capital ship, simple stuff for a captain such as yourself.\nDuring your journey you will encounter many "
         "things so don't be afraid to fail. Though i'm sure somebody with your skills will be fine. Good luck!")
-    global system
-    global area
+
     while system < 5:
         area = 1
         while area < 11:
             print("-------------------------------------------\n"
                   "You are in system {} area {}, You have {} Skill {} Health {} Luck {} Fuel and {} money".format
                   (system, area, skill, health, luck, fuel, money))
-            store = encounter.Store()
-            store.runLoop()
+            rand = random.randint(1, 100)
+            if rand < 65:
+                print("Enemy")
+                # areaencounter = encounter.Enemy()
+            elif rand < 75:
+                print(money)
+                print("Store")
+                money, fuel, repairToolAmount = encounter.store(money, fuel, max_fuel, repairToolAmount)
+
+            elif rand < 80:
+                print("Station")
+                # areaencounter = encounter.Station()
+            else:
+                print("NOTHING")
+            #areaencounter.runLoop()
             area += 1
         system += 1
 
--- encounter.py
+++ encounter.py
@@ -1,62 +1,40 @@
 from pip._vendor.distlib.compat import raw_input
 import random
 
-import Luthien
+def store(money, fuel, max_fuel, repairToolAmount):
+    print('Welcome to the store: what can we get for you?')
 
+    print('\n1.) buy more fuel - Cost 10 Salvage')
+    print('\n2.) Buy a repair tool - cost 10 Salvage')
+    print('\n3.) modify you ship - cost 100')
+    print('\n4.) leave the store')
+    while True:
+        selection = input('make a selection (1, 2, 3 or 4): ')
 
-class Encounter:
-    """The base encounter class"""
+        if selection == '1':
+            if fuel == max_fuel:
+                print('You already have the max amount of fuel.')
 
-    def ___init___(self, name, description):
-        self.name = name
-        self.description = description
+            else:
+                print('your fuel has been increased by one')
+                fuel = ++1
+                money = money- 10
+                print('your current fuel is: ' + fuel)
 
-    def ___str___(self):
-        return "{} {}".format(self.name, self.description)
+        elif selection == '2':
+            repairToolAmount = ++1
+            money = money - 10
+            print("Thank you for your purchase!")
 
-    def runLoop(self):
-        return
+        elif selection == '3':
+            print("Sorry currently out of stock")
 
+        elif selection == '4':
+            print("Hope to see you again soon!")
+            print(money)
+            return money, fuel, repairToolAmount
 
-class Store(Encounter):
-    def ___init___(self, name, description):
-        super().__init__(name, description)
-
-    def runLoop(self):
-        print('Welcome to the store: what can we get for you?')
-
-        print('\n1.) buy more fuel - Cost 10 Salvage')
-        print('\n2.) Buy a repair tool - cost 10 Salvage')
-        print('\n3.) modify you ship - cost 100')
-        print('\n4.) leave the store')
-        while True:
-            selection = input('make a selection (1, 2, 3 or 4): ')
-
-            if selection == '1':
-                if Luthien.fuel == Luthien.max_fuel:
-                    print('You already have the max amount of fuel.')
-
-                else:
-                    print('your fuel has been increased by one')
-                    Luthien.fuel = ++1
-                    Luthien.money = --10
-                    print('your current fuel is: ' + Luthien.fuel)
-
-            elif selection == '2':
-                Luthien.repairToolAmount = ++1
-                Luthien.money = --10
-                print("Thank you for your purchase!")
-
-            elif selection == '3':
-                print("Sorry currently out of stock")
-
-            elif selection == '4':
-                print("Hope to see you again soon!")
-                print(Luthien.money)
-                break
-
-
-class Station(Encounter):
+ class Station(Encounter):
     def ___init___(self, name, description):
         super().__init__(name, description)
 
