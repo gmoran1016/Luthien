554e64ddcb4c9eb0e07280940d5074d249900e12
zschaub <bdzschau@gmail.com>
2019-11-09 18:21:56 -0500

    Fixed EVERYTHING(but actually just store)

--- Luthien.py
+++ Luthien.py
@@ -8,16 +8,11 @@ def d6():
     return random.randint(1, 6)
 
 
-
-
-
 def main():
     max_skill = 6 + d6()
     skill = max_skill
     max_health = 12 + d6() + d6()
     health = max_health
-    max_luck = 6 + d6()
-    luck = max_luck
     max_fuel = 6 + d6()
     fuel = max_fuel
     weapon = LaserMk1
@@ -30,8 +25,7 @@ def main():
     print("First we will create your character and ship")
 
     print(
-        "Skill: {skill} Health: {health} Luck: {luck} Fuel: {fuel}"
-            .format(skill=skill, health=health, luck=luck, fuel=fuel))
+        "Skill: {} Health: {} Fuel: {}".format(skill, health, fuel))
 
     print(
         "\nYou find yourself the captain of the Starship Luthien. Congratulations on your promotion!\nAs i'm sure you "
@@ -45,8 +39,8 @@ def main():
         area = 1
         while area < 11:
             print("-------------------------------------------\n"
-                  "You are in system {} area {}, You have {} Skill {} Health {} Luck {} Fuel and {} money".format
-                  (system, area, skill, health, luck, fuel, money))
+                  "You are in system {} area {}, You have {} Skill {} Health {} Fuel and {} money".format
+                  (system, area, skill, health, fuel, money))
             rand = random.randint(1, 100)
             if rand < 65:
                 print("Enemy")
@@ -62,6 +56,11 @@ def main():
             else:
                 print("NOTHING")
             #areaencounter.runLoop()
+            if fuel == 0:
+                print("Sadly you have run out of and are stranded, you ended the game in system {} area {}".
+                      format(system, area))
+                exit(0)
+            fuel -= 1
             area += 1
         system += 1
 
