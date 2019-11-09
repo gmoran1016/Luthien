de873f368dd307151b5f8752e2a9a1e5498bce3e
zschaub <bdzschau@gmail.com>
2019-11-09 14:03:20 -0500

    Character Creation

--- Luthien.py
+++ Luthien.py
@@ -1,8 +1,5 @@
 import random
-skill = 6
-stamina = 12
-luck = 6
-fuel = 6
+
 
 def d6():
     d6var = random.randint(1,6)
@@ -11,6 +8,11 @@ def d6():
 def main():
     print("Welcome to Luthien a text based space adventure game!")
     print("First we will create your character and ship")
+    skill = 6 + d6()
+    stamina = 12 + d6() + d6()
+    luck = 6 + d6()
+    fuel = 6 + d6()
+    print("Skill: {skill} Stamina: {stamina} Luck: {luck} Fuel: {fuel}".format(skill=skill, stamina=stamina, luck=luck, fuel=fuel))
 
 
 if __name__ == "__main__":
