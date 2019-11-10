6bc6d6facf8f6d55b620c236d9f0bc37fe0f877d
zschaub <bdzschau@gmail.com>
2019-11-09 20:27:06 -0500

    Fixed Station

--- combat.py
+++ combat.py
@@ -30,3 +30,40 @@ def enemy(system, skill, health):
     print("You have encountered an enemy pirate ship with skill {} and health {}".format(enemyskill, enemyhealth))
     input("Press Enter to continue")
     return combat(skill, health, enemyskill, enemyhealth, "Pirate")
+
+def finalboss(skill, health, max_health, repairToolAmount):
+    enemyskill = 18
+    enemyhealth = 24
+    print("Ah it is Eridu AAAAAAAAAHHHHHHHHHHHHHHH")
+    print("Eridu's ship is strong and will not go down easy, he has a skill of {} and health of {}".format(enemyskill, enemyhealth))
+
+    damage = 2
+    while enemyhealth > 0:
+        print("**********************")
+        print("Your Ship Skill: {} Health: {} | {} Ship Skill: {} Health: {}".format(skill, health, "Eridu", enemyskill,
+                                                                                     enemyhealth))
+
+        if repairToolAmount > 0 and health < max_health:
+            selection = input("Would you like to use one of your repair tools (you have {} with a health of {} "
+                              "out of {}), y or n?".format(repairToolAmount, health, max_health))
+            if selection == 'y':
+                health += Luthien.d6() + Luthien.d6() - damage
+                repairToolAmount -= 1
+                if health > max_health:
+                    health = max_health
+        yourroll = Luthien.d6() + Luthien.d6() + skill
+        enemyroll = Luthien.d6() + Luthien.d6() + enemyskill
+
+        print("You rolled a skill of {} while Eridu rolled a skill of {}".format(yourroll, enemyroll))
+        if yourroll > enemyroll:
+            print("The enemy has taken {} damage".format(damage))
+            enemyhealth -= damage
+        elif enemyroll > yourroll:
+            print("You have taken {} damage".format(damage))
+            health -= damage
+            if health < 1:
+                print("Your ship has taken critical damage and was destroyed")
+                exit(0)
+        else:
+            print("You both are unable to damage each other")
+    print("{} has been destroyed!!!!!!!!!!!!!\n CONGRADULATION ON WINNING PLEASE PLAY AGAIN(or don't I don't care)".format("Eridu"))
