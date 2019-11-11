50a85874a11de0ae4aa0daed641547769f420e3d
Griffin Moran <gmoran1016@gmail.com>
2019-11-11 00:50:11 -0500

    Added Icon and new .exe

--- Luthien.py
+++ Luthien.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import encounter
 import combat
@@ -26,9 +27,6 @@ def main():
 """)
     print("Welcome to Luthien a text based Space Adventure Game!")
     print("By Brain Zschau & Griffin Moran")
-    print("\nFirst we will create your character and ship:")
-    print("Skill: {} Health: {} Fuel: {}".format(skill, health, fuel))
-
     print(
         "\nYou find yourself the captain of the Starship Luthien. Congratulations on your promotion!\nAs i'm sure you "
         "are aware you are on the run from the Eridu Empire, The most evil empire in the galaxy!")
@@ -36,7 +34,9 @@ def main():
         "\nYour goal is simple, navigate the various star systems of the universe and find and destroy the Eridu "
         "Capital ship, simple stuff for a captain such as yourself.\nDuring your journey you will encounter many "
         "things so don't be afraid to fail. Though i'm sure somebody with your skills will be fine. Good luck!")
-    ##input("\nPress enter to continue")##
+    input("\nPress enter to continue")
+    print("\nFirst we will create your character and ship:")
+    print("Skill: {} Health: {} Fuel: {}".format(skill, health, fuel))
 
     test = input("\nWould you like to Start with 50 extra salvage (1) \nor \n2 Extra repair tools (2): ")
     if test == '1':
